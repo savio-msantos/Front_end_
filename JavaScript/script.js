1fac37d081d985792c26a084bf5fa02f6e737375
Sávio Monteiro <saviomonteiro2005@gmail.com>
2026-05-22 20:33:13 -0500

    Atividades Realizadas03

---
+++ JavaScript/script.js
@@ -0,0 +1,83 @@
+//Console
+console.log("Boa noite, hoje é sexta-feira!")
+console.error("Este é um Erro")
+console.warn("Este é um aviso")
+
+//Variáveis
+var x = 10
+
+//Variáveis modernas
+let y = 15
+const z = 20
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+//Tipos de dados
+
+//Texto
+
+const name = "Sávio Monteiro dos Santos"
+
+const shirtsQty = 4
+
+console.log(shirtsQty)
+
+console.log(typeof shirtsQty)
+
+//Número Decimal
+
+const decimal = 20.1
+
+console.log(decimal)
+
+console.log(typeof decimal)
+
+//Booleano (True ou False)
+
+const isApproved = false
+
+console.log(isApproved)
+
+console.log(typeof isApproved)
+
+//Null
+
+let surname = null
+
+console.log(surname)
+
+console.log(typeof surname)
+
+//Array
+
+let languages = ['JavaScript','PHP','Python']
+
+console.log(languages)
+
+console.log(typeof languages)
+
+//Object
+
+const user = {email: "saviomonteiro2005@gmail.com", password: "tente123", age: 20}
+
+console.log(user)
+
+console.log(typeof user)
+
+//Método de String
+
+const fullName = 'Sávio Monteiro dos Santos'
+
+console.log(fullName.length)
+
+console.log(typeof fullName)
+
+//Método de String list
+
+const array = fullName.split(' ')
+
+console.log(array)
+
+console.log(typeof )
